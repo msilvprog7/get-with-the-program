ad612ce19f61d0d71ceea56274fa035b23a7f2e0
Joe Barrow <joseph.d.barrow@gmail.com>
2015-11-14 15:52:34 -0500

    Hex_to_rgb added

---
+++ editor/levels.py
@@ -0,0 +1,11 @@
+from constants import *
+import pygame
+
+class Levels(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Levels, self).__init__()
+
+
+class LevelBox(pygame.sprite.Sprite):
+    def __init__(self):
+        super(LevelBox, self).__init__()
